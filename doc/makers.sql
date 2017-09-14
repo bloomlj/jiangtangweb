d5f9a9d8c8fe076ef464aaaa666d8fb293283ea8
bloomlj <kollocc@live.com>
2017-09-14 13:54:10 +0800

    update tables

--- doc/makers.sql
+++ doc/makers.sql
@@ -1,3 +1,69 @@
-﻿INSERT INTO public.makers(
-             login, password, name, about,created_at,updated_at)
-    VALUES ('josh', '1234', 'lijun', 'mmmmm',now(),now());
+-- Table: public.makers
+
+-- DROP TABLE public.makers;
+
+CREATE TABLE public.makers
+(
+  id integer NOT NULL DEFAULT nextval('makers_id_seq'::regclass),
+  login character varying(100),
+  password character varying(100),
+  name character varying(50),
+  about text,
+  created_at timestamp without time zone,
+  updated_at timestamp without time zone,
+  avatar character varying(255),
+  CONSTRAINT makers_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.makers
+  OWNER TO postgres;
+
+
+  -- Table: public.files
+
+  -- DROP TABLE public.files;
+
+  CREATE TABLE public.files
+  (
+    id integer NOT NULL DEFAULT nextval('files_id_seq'::regclass),
+    maker_id integer,
+    org_name character varying(255),
+    category character varying(50),
+    mimetype character varying(50),
+    filename character varying(255),
+    note character varying(255),
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone
+  )
+  WITH (
+    OIDS=FALSE
+  );
+  ALTER TABLE public.files
+    OWNER TO postgres;
+
+
+
+    -- Table: public.things
+
+    -- DROP TABLE public.things;
+
+    CREATE TABLE public.things
+    (
+      id integer NOT NULL DEFAULT nextval('things_id_seq'::regclass),
+      maker_id integer,
+      title character varying(255),
+      cover character varying(255),
+      abstract text,
+      content jsonb,
+      category jsonb,
+      copyright character varying(50),
+      created_at timestamp without time zone,
+      updated_at timestamp without time zone
+    )
+    WITH (
+      OIDS=FALSE
+    );
+    ALTER TABLE public.things
+      OWNER TO postgres;
